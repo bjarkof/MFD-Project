b30145d30ba3ef7ab70cc0f2f322dcb69bf9ba8c
Bjarke Mulbjerg <bjarkemulbjerg12@gmail.com>
2016-09-09 20:06:28 +0200

    Added main.js

---
+++ js/main.js
@@ -0,0 +1,78 @@
+jQuery.noConflict();
+
+jQuery(document).ready(function( $ ){
+
+/// RESPONSIVE MENU ///
+console.log("hej");
+  $('.menutoggle').on('click', function(){
+   $('.menu').slideToggle(200);
+  });
+  $(window).resize(function(){
+    var w = $(window).width();
+    if(w > 1000 && $('.menu').is(':hidden')){
+      $('.menu').removeAttr('style');
+    }
+  });
+
+/// SLIDER ///
+
+  $('.slider').slick({
+    autoplay: false,
+    appendArrows: $('.slider'),
+    nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
+    prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>'
+  });
+
+  $( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        tooltip.text("$" + ui.values[ 0 ]);
+        tooltip2.text("$" + ui.values[ 1 ]);
+      }
+    })
+
+    var tooltip = $('<div class="tooltip" />');
+    var tooltip2 = $('<div class="tooltip" />');
+
+    // Sets initial value
+    tooltip.text( "$" + $( "#slider-range" ).slider( "values", 0 ));
+    $(".ui-slider-handle:first-of-type").prepend(tooltip);
+    tooltip2.text( "$" + $( "#slider-range" ).slider( "values", 1 ));
+    $(".ui-slider-handle:last-of-type").prepend(tooltip2);
+
+  });
+
+/// PRODUCT GALLERY ///
+
+    $('.thumbnail').on('click', function(){
+      $('.select-img').removeClass('select-img');
+      $(this).addClass('select-img');
+    });
+
+    $('.thumbnail').on('click', function() {
+    var thmb = this;
+    var src = this.src;
+    $('.big-prod-image').fadeOut(400,function(){
+        thmb.src = this.src;
+        $(this).fadeIn(400)[0].src = src;
+    });
+});
+
+/// TABS ///
+
+  $( function() {
+    $( "#tabs" ).tabs({
+      hide: 200,
+      show: 500
+    });
+  } );
+
+/// TESTING ///
+
+
+
+});
